24bff693aba0c8004c910f863709bdda173563a5
Paulo Junior <3rd.paulo.junior@b2wdigital.com>
2015-03-31 16:32:44 -0300

    Implementação - Apresentação - Framework Jquery DataTables

---
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO.ORCAMENTO/Controllers/LancamentoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2WTI.PCFTI.APRESENTACAO.ORCAMENTO.Controllers
+{
+    public class LancamentoController : Controller
+    {
+        // GET: Lancamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO.ORCAMENTO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="B2WTI.PCFTI.APRESENTACAO.ORCAMENTO.MvcApplication" Language="C#" %>
