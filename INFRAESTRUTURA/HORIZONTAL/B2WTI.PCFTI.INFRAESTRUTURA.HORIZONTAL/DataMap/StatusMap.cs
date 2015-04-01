bce9c11ade194df779e52ddfa79abe7d7716cff2
Fabio Braga <fabio.braga@b2wdigital.com>
2015-04-01 12:34:59 -0300

    Modelo MAP commit 2

---
+++ INFRAESTRUTURA/HORIZONTAL/B2WTI.PCFTI.INFRAESTRUTURA.HORIZONTAL/DataMap/StatusMap.cs
@@ -0,0 +1,33 @@
+﻿
+namespace B2WTI.PCFTI.INFRAESTRUTURA.HORIZONTAL.DataMap
+{
+    using B2WTI.PCFTI.DOMINIO.Model.Global;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StatusMap : EntityTypeConfiguration<Status>
+    {
+        public StatusMap()
+        {
+            this.ToTable("Status");
+
+            this.HasKey(t => t.StatusId);
+
+            this.Property(t => t.StatusId)
+                .HasColumnName("StatusId");
+
+            this.Property(t => t.Descricao)
+                .HasColumnName("Descricao")
+                .HasColumnType("varchar")
+                .HasMaxLength(250);
+               
+            this.Property(t => t.Ativo)
+                .HasColumnName("Ativo");
+        }
+
+    }
+}
---
+++ INFRAESTRUTURA/HORIZONTAL/B2WTI.PCFTI.INFRAESTRUTURA.HORIZONTAL/DataMap/TipoServicoMap.cs
@@ -0,0 +1,33 @@
+﻿
+namespace B2WTI.PCFTI.INFRAESTRUTURA.HORIZONTAL.DataMap
+{
+    using B2WTI.PCFTI.DOMINIO.Model.Global;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TipoServicoMap : EntityTypeConfiguration<TipoServico>
+    {
+        public TipoServicoMap()
+        {
+            this.ToTable("TipoServico");
+
+            this.HasKey(t => t.TipoServicoId);
+
+            this.Property(t => t.TipoServicoId)
+                .HasColumnName("TipoServicoId");
+
+            this.Property(t => t.Descricao)
+                .HasColumnName("Descricao")
+                .HasColumnType("varchar")
+                .HasMaxLength(250);
+               
+            this.Property(t => t.Ativo)
+                .HasColumnName("Ativo");
+        }
+
+    }
+}
