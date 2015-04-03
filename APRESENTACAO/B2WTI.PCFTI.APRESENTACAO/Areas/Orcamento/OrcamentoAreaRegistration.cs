744aff9214835d9e06e358554cb9a980acbb0b23
Fabio Braga <fabio.braga@b2wdigital.com>
2015-04-03 20:40:15 -0300

    Novo projeto de apresentação estruturado para áreas e com namespace correto

---
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Areas/Orcamento/OrcamentoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace B2WTI.PCFTI.APRESENTACAO.Areas.Orcamento
+{
+    public class OrcamentoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Orcamento";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Orcamento_default",
+                "Orcamento/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="B2WTI.PCFTI.APRESENTACAO.MvcApplication" Language="C#" %>
