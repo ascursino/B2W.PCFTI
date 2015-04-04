34b717ca1566cfb46242a8328dd396209f80adf5
Fabio Braga <fabio.braga@b2wdigital.com>
2015-04-04 00:17:03 -0300

    Include ResponsavelBuild

---
+++ APLICACAO/B2WTI.PCFTI.APLICACAO/Operacao/Build/ResponsavelBuild.cs
@@ -0,0 +1,16 @@
+﻿
+namespace B2WTI.PCFTI.APLICACAO.Operacao.Build
+{
+    using APLICACAO.Modulo.Cadastro;
+    using INFRAESTRUTURA.HORIZONTAL;
+    using INFRAESTRUTURA.TRANSVERSAL.DataContexts;
+    using INFRAESTRUTURA.TRANSVERSAL.Repositories;
+    using INFRAESTRUTURA.TRANSVERSAL.UnitOfWork;
+    using DOMINIO.Model.Global;
+    using System;
+    using System.Collections.Generic;
+
+    public class ResponsavelBuild
+    {
+    }
+}
