5e1b42f1bf6f413a36126d7562bcfc8710f851f1
Fabio Braga <fabio.braga@b2wdigital.com>
2015-04-05 11:31:14 -0300

    Add Execute in Services

---
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO.SERVICES/Exe/BlocoExecute.cs
@@ -0,0 +1,25 @@
+﻿
+namespace B2WTI.PCFTI.APRESENTACAO.SERVICES.Exe
+{
+    using INFRAESTRUTURA.TRANSVERSAL.DTO.Modulo.Cadastro.ViewModel;
+    using B2WTI.PCFTI.APLICACAO.Operacao;
+    using DOMINIO.Model.Global;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Map;
+
+    public static class BlocoExecute
+    {
+        static Execute Executar = new Execute();
+
+        public static List<BlocoView> ListarTodosOsBlocos()
+        {
+            List<BlocoView> blocos = Executar.Cadastro.Bloco.ListarBlocos().DeDominParaView();
+            return blocos;
+        }
+
+    }
+}
