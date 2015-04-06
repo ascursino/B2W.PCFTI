b1b0b4a82e97bee3b31384eea9b7684732cb769e
Paulo Junior <3rd.paulo.junior@b2wdigital.com>
2015-04-06 16:10:26 -0300

    Acerto, Implementação do Arrow Editable

--- APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Areas/Orcamento/Controllers/LancamentoController.cs
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Areas/Orcamento/Controllers/LancamentoController.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿
+using B2WTI.PCFTI.INFRAESTRUTURA.TRANSVERSAL.DTO.Modulo.Orcamento;
 
 namespace B2WTI.PCFTI.APRESENTACAO.Areas.Orcamento.Controllers
 {
+    using B2WTI.PCFTI.APRESENTACAO.SERVICES.Exe;
+    using INFRAESTRUTURA.TRANSVERSAL.DTO.Modulo.Cadastro.ViewModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
     public class LancamentoController : Controller
     {
         // GET: Orcamento/Lancamento
