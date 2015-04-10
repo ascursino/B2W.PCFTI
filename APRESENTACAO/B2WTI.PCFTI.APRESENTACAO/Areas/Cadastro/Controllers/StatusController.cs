55f1bd4fc36d092d3c0b6352d22cc87aeec7b285
Paulo Junior <3rd.paulo.junior@b2wdigital.com>
2015-04-10 19:27:09 -0300

    Criação do Execute, Map, Build do Status

--- APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Areas/Cadastro/Controllers/StatusController.cs
+++ APRESENTACAO/B2WTI.PCFTI.APRESENTACAO/Areas/Cadastro/Controllers/StatusController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using B2WTI.PCFTI.APRESENTACAO.SERVICES.Exe;
+using B2WTI.PCFTI.INFRAESTRUTURA.TRANSVERSAL.DTO.Modulo.Cadastro.ViewModel;
 
 namespace B2WTI.PCFTI.APRESENTACAO.Areas.Cadastro.Controllers
 {
@@ -11,7 +13,68 @@ namespace B2WTI.PCFTI.APRESENTACAO.Areas.Cadastro.Controllers
         // GET: Cadastro/Status
         public ActionResult Index()
         {
-            return View();
+            List<StatusView> lista = StatusExecute.ListarTodosOsStatus();
+            return View(lista);
+        }
+
+        [HttpPost]
+        // POST: Cadastro/Status/Novo
+        public ActionResult Index(string filtro)
+        {
+            ViewData["filtro"] = filtro;
+            List<StatusView> lista = StatusExecute.BuscarStatus(filtro);
+            return View("Index", lista);
+        }
+
+        [HttpGet]
+        // GET: Cadastro/Status/Novo
+        public ActionResult Novo()
+        {
+            return View("Novo", new StatusView());
+        }
+
+        [HttpPost]
+        // POST: Cadastro/Status/Novo
+        public ActionResult Novo(StatusView status)
+        {
+            status.Ativo = true;
+            status = StatusExecute.CriarNovoStatus(status, User.Identity.Name.ToString(), DateTime.Now);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        // GET: Cadastro/Status/Editar
+        public ActionResult Editar(Guid Id)
+        {
+            StatusView item = StatusExecute.CarregarStatus(Id);
+            return View("Editar", item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // POST: Cadastro/Status/Editar
+        public ActionResult Editar(StatusView status)
+        {
+            status = StatusExecute.EditarStatus(status, User.Identity.Name.ToString(), DateTime.Now);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        // GET: Cadastro/Status/Excluir
+        public ActionResult Excluir(Guid Id)
+        {
+            StatusView item = StatusExecute.CarregarStatus(Id);
+            return View("Excluir", item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // POST: Cadastro/Status/Excluir
+        public ActionResult Excluir(StatusView status)
+        {
+            bool result = StatusExecute.ExcluirStatus(status, User.Identity.Name.ToString(), DateTime.Now);
+            return RedirectToAction("Index");
         }
     }
 }
